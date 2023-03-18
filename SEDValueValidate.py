bf7886d525aeb82f8a0e6598270d8ac558f5e0b8
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2023-03-18 15:02:27 +0300

    adding CSVValueLookup

--- SEDValueValidate.py
+++ SEDValueValidate.py
@@ -1,4 +1,6 @@
-
+import csv
+from SEDFileManager import path
+from CLIMessage import CLIMessage
 class VV():
     def __init__(self, val, valType): 
         self.val = val
@@ -25,3 +27,20 @@ class VV():
                 return False
         else:
             return False
+
+class CSVValueLookup():
+    def __init__(self, csvFile, value):
+        if path(csvFile).exist():
+            with open(csvFile, 'r') as f:
+                try: 
+                    csvReader = csv.reader(csvFile)
+                    header = next(csvReader)
+                    for row in csvReader:
+                        if value in row:
+                            return header, row
+                except:
+                    CLIMessage('Uable to read the {} file'.format(csvFile), 'E')
+                    return 0, 0
+        else: 
+            CLIMessage('The path {} seems not exist'.format(csvFile), 'E')
+            return 0, 0
