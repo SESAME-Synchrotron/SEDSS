936bd4e48ea3a300dd2650303a14fa0c35f47896
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2023-07-04 11:42:52 +0300

    update

---
+++ C++/use_example.cpp
@@ -0,0 +1,41 @@
+#include "timeFunction.h"
+#include "message.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	
+	CLIMessage cliMsg;
+	LogMessage logMsg; 
+	std::cout<<std::endl;
+	std::cout<<"################## Log Messages Examples ##################"<<std::endl;
+
+	logMsg.show("This is an information log message", "info");
+	logMsg.show("This is a warning log message", "w");
+	logMsg.show("This is an error log message", "e");
+	logMsg.show("This is a critical log message ", "c");
+	
+	std::cout<<std::endl;
+	std::cout<<"################## CLI Messages Examples ##################"<<std::endl;
+
+	cliMsg.show("This is info CLI Message", "I");
+	cliMsg.show("This is worning CLI Message", "W");
+	cliMsg.show("This is error CLI Message", "E");
+	cliMsg.show("This is critical CLI Message", "C");
+	cliMsg.show("This is undefined CLI Message", "U");
+
+	std::cout<<std::endl;
+	std::cout<<"################## Time Function Examples ##################"<<std::endl;
+	
+	std::string timenow {timeFunction(timeFunction::precision::s).getTimeNow()};
+	std::cout << "Time now in seconds precistion: " << timenow <<std::endl;
+	timenow = timeFunction(timeFunction::precision::mls).getTimeNow();
+	std::cout << "Time now in milliseconds: " << timenow <<std::endl;
+	timenow = timeFunction(timeFunction::precision::mcs).getTimeNow();
+	std::cout << "Time now in microseconds: " << timenow <<std::endl;
+	timenow = timeFunction(timeFunction::precision::ns).getTimeNow();
+	std::cout << "Time now in nanoseconds: " << timenow <<std::endl;
+
+	return 0;
+}
