a86173502fa1b9723e2f7c626c90809770dd877c
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-10-26 17:03:04 +0300

    add getProposalInfo method

--- SEDFileManager.py
+++ SEDFileManager.py
@@ -1,53 +1,76 @@
+import os
+import re
 import json
 import ntpath
-import os
-from SEDSS.CLIMessage import CLIMessage
+import time
+import pandas as pd
+
 from epics import PV
+from SEDSS.CLIMessage import CLIMessage
 
 class readFile():
 	def __init__(self, fullFileName):
 		"""
-		A simple and very standard method to read and print JSON files. 
-		JFName: JSON File Name
-		JFPath: JSON File Path
+		FName: File Name
+		FPath: File Path
 		"""
-		self.JFPath, self.JFName = ntpath.split(fullFileName)
+		self.FPath, self.FName = ntpath.split(fullFileName)
 		#print (self.fName)
 
 	def readJSON(self, Print="N"):
+		"""
+		A simple and very standard method to read and print JSON files.
+		"""
 		try:
-			with open(self.JFPath+"/"+self.JFName, "r") as jsonFile:
+			with open(self.FPath+"/"+self.FName, "r") as jsonFile:
 				jsonFileContent  = json.load(jsonFile)
 				jsonFile.close()
 				if Print.lower() in ("y", "yes"):
-					CLIMessage("Printing {} file contents | start of file".format(self.JFName))
+					CLIMessage("Printing {} file contents | start of file".format(self.FName))
 					print (json.dumps(jsonFileContent, indent = 4, sort_keys=True))
-					CLIMessage("Printing {} file contents | End of file".format(self.JFName))
+					CLIMessage("Printing {} file contents | End of file".format(self.FName))
 				return jsonFileContent
 		except Exception as e:
-			CLIMessage ("{} :: load error".format(self.JFName), "E")
+			CLIMessage ("{} :: load error".format(self.FName), "E")
 			print(e)
 
+	def getProposalInfo(self, proposal):
+		"""
+		A simple and very standard method to get the proposal info.
+		"""
+
+		df = pd.read_csv(self.FPath+"/"+self.FName)
+
+		info = df[df['Proposal'].eq(proposal)]
+
+		if not info.empty:
+			proposer = info['Proposer'].values[0]
+			email = info['Email'].values[0]
+			title = info['Title'].values[0]
+
+			return proposer, email, title
+		else:
+			return "Proposer", "proposer@sesame.org.jo", "User Experiment"
 
 class path ():
 	def __init__ (self, path, operatingSys = 'Linux', beamline = None, proposal = None, semester = None):
 		"""
-		Path class impliments the following: 
-		- Paths check -> retutrns true or false. 
+		Path class impliments the following:
+		- Paths check -> retutrns true or false.
 		- Paths creartion -> overwrite yes or no, force creation
 		"""
 		self.path = path
 		self.operatingSys = operatingSys
 		self.beamline = beamline
-		self.proposal = proposal 
+		self.proposal = proposal
 		self.semester = semester
 
 	def exist (self):
 		return os.path.exists(self.path)
 
-	def create (self): 
+	def create (self):
 		if not self.exist():
-			try: 
+			try:
 				os.mkdir(self.path)
 			except:
 				CLIMessage ('Unable to create the path !!', 'E')
@@ -63,7 +86,7 @@ class path ():
 		# 	return propPath
 		# else:
 		# 	CLIMessage ('SEDFileManager.getPropPath():: The path {} is not exist'.format(propPath), 'W')
-		# 	#return False 
+		# 	#return False
 
 	def getIHPath(self):
 		beamline = self.beamline
@@ -82,13 +105,13 @@ class fileName:
 
 		SED_Pattern = r'[\s]|[^\w\-]'			#  file path, spaces, special characters except dashes are not allowed
 		pathValidation = bool(re.search(SED_Pattern,fileName))
-		
+
 		if pathValidation or fileName.startswith("-"):
 			return True
 
 	@staticmethod
 	def SED_fileName(filePath, fileName, beamline = "SED"):
-			
+
 			if not re.match(r'\S', fileName): 	# To check a line whether it starts with a non-space character or not.
 				fileName = beamline
 			SEDTimeStamp = str(time.strftime("%Y%m%dT%H%M%S"))
